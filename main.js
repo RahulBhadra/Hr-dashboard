bdf72e49e3c53dc4a4c7dbeeb3954cf6dfcbdded
Rahul <rahulbhadra525@gmail.com>
2021-03-04 12:09:37 +0530

    done

---
+++ main.js
@@ -0,0 +1,22 @@
+  var options = {
+          series: [44, 55],
+          labels: ['Hired', 'Rejected'],
+          chart: {
+          type: 'donut',
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+      
